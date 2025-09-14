4312e4da291fab53f05218f35de152596b5333e4
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-09-15 04:27:49 +0700

    Update AppLayout.vue

--- services/ui-service/src/components/AppLayout.vue
+++ services/ui-service/src/components/AppLayout.vue
@@ -29,7 +29,7 @@
                     <MenuItem
                       v-for="menu in serviceMenus"
                       :key="menu.id"
-                      :to="menu.url"
+                      to="#"
                       v-slot="{ active }"
                     >
                       <router-link
