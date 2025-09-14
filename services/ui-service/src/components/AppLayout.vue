80805513be245abeee7aaaf2f3c9f36adc7e23d7
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-09-15 05:20:34 +0700

    Update AppLayout.vue

--- services/ui-service/src/components/AppLayout.vue
+++ services/ui-service/src/components/AppLayout.vue
@@ -52,7 +52,7 @@
                         v-if="menu && menu.url"
                         :class="[
                           active ? 'bg-gray-100' : '',
-                          'block px-4 py-2 text-sm text-gray-700 cursor-pointer'
+                          'block px-2 py-2 text-sm text-gray-700 cursor-pointer'
                         ]"
                         @click="selectService(menu)"
                       >
