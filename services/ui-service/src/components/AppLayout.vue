6b7b3d1ae48fcd3474e31880605f29ef8e95f9cc
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-09-15 05:25:00 +0700

    Update AppLayout.vue

--- services/ui-service/src/components/AppLayout.vue
+++ services/ui-service/src/components/AppLayout.vue
@@ -13,7 +13,7 @@
               <!-- XERPIUM Brand Menu -->
               <Menu v-if="serviceMenus.length > 0" as="div" class="relative">
                 <MenuButton class="text-xl font-bold inline-flex items-center border-primary-500 text-gray-900 router-link-exact-active border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-2 px-1 border-b-2 font-medium text-sm transition-colors">
-                  XERPIUM
+                  <Squares2X2Icon/>
                   <ChevronDownIcon class="ml-1 h-4 w-4" />
                 </MenuButton>
                 <transition
@@ -331,7 +331,7 @@
 import { ref, computed, onMounted, watch } from 'vue'
 import { useRouter } from 'vue-router'
 import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
-import { Bars3Icon, XMarkIcon, ChevronDownIcon } from '@heroicons/vue/24/outline'
+import { Bars3Icon, XMarkIcon, ChevronDownIcon,Squares2X2Icon } from '@heroicons/vue/24/outline'
 import { useAuthStore } from '@/stores/auth'
 import { useMenuStore } from '@/stores/menu'
 import NotificationCenter from './NotificationCenter.vue'
