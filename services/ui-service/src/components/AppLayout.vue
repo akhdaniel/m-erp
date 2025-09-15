a0054c3edd64d5cbbd77f7db8fbf66ca94ec3ba9
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-09-16 04:38:07 +0700

    Update AppLayout.vue

--- services/ui-service/src/components/AppLayout.vue
+++ services/ui-service/src/components/AppLayout.vue
@@ -63,8 +63,7 @@
                     </MenuItem>
                   </MenuItems>
                 </transition>
-                <!-- Theme Selector -->
-                <ThemeSelector />                 
+            
               </Menu>
               
               <div class="text-xl text-bold text-white">{{activeService}}</div>
@@ -132,11 +131,11 @@
               
             </div>
           </div>
-          
+          <!-- Theme Selector -->
+          <ThemeSelector />               
           <!-- User menu -->
           <div class="hidden sm:ml-6 sm:flex sm:items-center space-x-4">
-            
-            
+             
             <Menu as="div" class="ml-3 relative">
               <div>
                 <MenuButton class="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
