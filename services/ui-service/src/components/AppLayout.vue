25f74f9da0887509590b39b47fe9010a02655954
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-09-15 15:18:55 +0700

    Update AppLayout.vue

--- services/ui-service/src/components/AppLayout.vue
+++ services/ui-service/src/components/AppLayout.vue
@@ -85,7 +85,7 @@
                 
                 <!-- Dropdown menu for service sub-menus -->
                 <Menu v-else-if="menu.item_type === 'dropdown' && menu.children && menu.children.length > 0" as="div" class="relative">
-                  <MenuButton class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-2 px-1 border-b-2 font-medium text-sm transition-colors inline-flex items-center">
+                  <MenuButton class="text-gray-500 hover:text-gray-700 hover:background-gray-700 whitespace-nowrap font-medium text-sm transition-colors px-3 py-0">
                     <span v-if="menu.icon" class="mr-1">
                       <i :class="menu.icon"></i>
                     </span>
