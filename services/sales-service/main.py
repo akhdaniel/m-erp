5356781b29c96824c02f693b2da9a552d44aa9a1
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-09-19 13:39:00 +0700

    Update main.py

--- services/sales-service/main.py
+++ services/sales-service/main.py
@@ -33,7 +33,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('uvicorn')
 
 # Create FastAPI application
 app = FastAPI(
