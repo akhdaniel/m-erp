e2e299b81002bc13bc8c1da4c94af9abdd4ec984
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-09-19 10:25:45 +0700

    Update menu_registration_client.py

--- services/shared/menu_registration_client.py
+++ services/shared/menu_registration_client.py
@@ -196,7 +196,7 @@ class MenuRegistrationClient:
                             headers=self._get_headers()
                         )
 
-                        logger.info('parent_response', parent_response)
+                        logger.info(f'parent_response====={parent_response}')
                         
                         if parent_response.status_code == 200:
                             parent_data = parent_response.json()
