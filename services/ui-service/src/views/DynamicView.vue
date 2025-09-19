b172c59504b6e6e11e208507b6b89026ee365598
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-09-20 05:15:43 +0700

    Update DynamicView.vue

--- services/ui-service/src/views/DynamicView.vue
+++ services/ui-service/src/views/DynamicView.vue
@@ -208,7 +208,7 @@ function getSchemaEndpoint(): string {
   else if(/\d+$/.test(path)) {
     // /inventory/products/14 => /inventory/products/ui-schemas/form
     const segments = path.split("/").filter(segment => segment !== "");
-    return '/api/v1/'+segments[0]+'/'+segments[1]+'/ui-schemas/form'
+    return '/api/v1/'+segments[0]+'/'+segments[1]+'/ui-schemas/view'
   }
   else
     return '/api/v1'+path+'/ui-schemas/'+viewType.value
