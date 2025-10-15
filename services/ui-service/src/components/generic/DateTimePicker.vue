6ea38668cda636a560b5aee4b559735b75755c59
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-10-15 09:44:33 +0700

    Update DateTimePicker.vue

--- services/ui-service/src/components/generic/DateTimePicker.vue
+++ services/ui-service/src/components/generic/DateTimePicker.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="datetime-picker" ref="pickerContainer">
     <label :for="id" class="block text-sm font-medium text-gray-700">
-      {{ label }}
+      {{ label }} dateyim
       <span v-if="required" class="text-red-500">*</span>
     </label>
     
