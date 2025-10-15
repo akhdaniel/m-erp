d9448e629d63c725b73881b42609707c009f1e86
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-10-15 17:42:37 +0700

    Update order.py

--- services/sales-service/sales_module/models/order.py
+++ services/sales-service/sales_module/models/order.py
@@ -140,6 +140,7 @@ class SalesOrder(CompanyBusinessObject):
     requires_approval = Column(Boolean, nullable=False, default=False)
     is_dropship = Column(Boolean, nullable=False, default=False)
     is_backorder_allowed = Column(Boolean, nullable=False, default=True)
+    hold_status = Column(Boolean, nullable=False, default=False)
     
     # Customer service information
     customer_po_number = Column(String(100), nullable=True, index=True)
