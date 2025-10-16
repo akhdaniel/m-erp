8cd32758e3a278932e76b4157a671a5da2af2174
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-10-16 17:35:44 +0700

    Update DateTimePicker.vue

--- services/ui-service/src/components/generic/DateTimePicker.vue
+++ services/ui-service/src/components/generic/DateTimePicker.vue
@@ -241,7 +241,7 @@ const calendarDays = computed(() => {
 
 // Watch for modelValue changes
 watch(() => props.modelValue, (newValue) => {
-  console.log('props.modelValue',props.modelValue)
+  console.log('props',props)
   console.log('newValue',newValue)
   if (newValue) {
     displayValue.value = newValue
