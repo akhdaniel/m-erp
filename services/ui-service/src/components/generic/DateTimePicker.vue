1148f2358e62e519f9cef5d07b25eac9717a941d
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-10-16 15:34:12 +0700

    Update DateTimePicker.vue

--- services/ui-service/src/components/generic/DateTimePicker.vue
+++ services/ui-service/src/components/generic/DateTimePicker.vue
@@ -506,7 +506,7 @@ onMounted(() => {
   document.addEventListener('keydown', handleKeydown)
 
 
-  console.log('prop.modelValues',props.modelValue)
+  console.log('props',props)
   
   if (props.modelValue) {
     displayValue.value = props.modelValue
