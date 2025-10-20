363936dd8c697e5aac38ef2e16f87a1dc2c95acb
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-10-21 04:10:03 +0700

    sales_order_line_items

--- services/sales-service/migrations/versions/20251020_110000_add_quantity_ordered.py
+++ services/sales-service/migrations/versions/20251020_110000_add_quantity_ordered.py
@@ -1,4 +1,4 @@
-"""Add quantity_ordered field to sales_order_line_items table
+"""Add quantity_ordered field to sales_orders table
 
 Revision ID: 20251020_110000
 Revises: 20251014_170000
@@ -17,10 +17,10 @@ depends_on = None
 
 
 def upgrade() -> None:
-    """Add quantity_ordered column to sales_order_line_items table."""
-    # Add due_date column to sales_order_line_items table
-    op.add_column('sales_order_line_items', sa.Column('quantity_ordered', sa.DateTime(), nullable=True, index=True))
+    """Add quantity_ordered column to sales_orders table."""
+    # Add due_date column to sales_orders table
+    op.add_column('sales_orders', sa.Column('quantity_ordered', sa.DateTime(), nullable=True, index=True))
 
 def downgrade() -> None:
-    """Remove quantity_ordered column from sales_order_line_items table."""
-    op.drop_column('sales_order_line_items', 'quantity_ordered')
+    """Remove quantity_ordered column from sales_orders table."""
+    op.drop_column('sales_orders', 'quantity_ordered')
---
+++ services/sales-service/migrations/versions/20251020_120000_add_line_quantity_ordered.py
@@ -0,0 +1,26 @@
+"""Add quantity_ordered field to sales_order_line_items table
+
+Revision ID: 20251020_120000
+Revises: 20251020_110000
+Create Date: 2025-10-14 15:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '20251020_120000'
+down_revision = '20251020_110000'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    """Add quantity_ordered column to sales_order_line_items table."""
+    # Add due_date column to sales_order_line_items table
+    op.add_column('sales_order_line_items', sa.Column('quantity_ordered', sa.DateTime(), nullable=True, index=True))
+
+def downgrade() -> None:
+    """Remove quantity_ordered column from sales_order_line_items table."""
+    op.drop_column('sales_order_line_items', 'quantity_ordered')
