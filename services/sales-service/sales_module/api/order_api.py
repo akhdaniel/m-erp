0041d9e598fd5e52b6d24d666a29b35f819aae04
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-10-20 17:35:56 +0700

    Update order_api.py

--- services/sales-service/sales_module/api/order_api.py
+++ services/sales-service/sales_module/api/order_api.py
@@ -656,8 +656,9 @@ async def update_order(
     try:
         logger.info(f'put:update_order order_id={order_id}, company_id={company_id}')
         order = order_service.get_by_id_or_raise(SalesOrder, order_id, company_id)
-        updated_order = order_service.update(order, order_id, order_data.dict(exclude_unset=True), user_id, company_id)
+        logger.info(f'order={order} order_id={order_id} order_data={order_data}')
 
+        updated_order = order_service.update(order, order_id, order_data.dict(exclude_unset=True), user_id, company_id)
         logger.info(f'updated_order={updated_order}')
         order_service.commit()
         
